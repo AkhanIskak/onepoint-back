c94b798f08687855e25ba60e9eaadce541554d59
Zhan <zaubakirov@beeline.kz>
2022-04-29 10:43:42 +0600

    conspect model

---
+++ models/conspect.mjs
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const conspectSchema = new Schema({
+    name: String,
+    attachments: [{base64: String, date: {type: Date, default: Date.now}}],
+    createTs: {type: Date, default: Date.now},
+    comments: [{body: String, date: Date}],
+    createdBy: String,
+    rating: Number
+});
+
+const Conspect = mongoose.model('Conspect', conspectSchema);
+
+export default Conspect;
