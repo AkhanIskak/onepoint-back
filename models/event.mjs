9f3f9bd43fe416aaada9b53b8da1ae823ac05583
Zhan <zaubakirov@beeline.kz>
2022-04-29 23:03:22 +0600

    eventSchema

--- models/event.mjs
+++ models/event.mjs
@@ -3,13 +3,27 @@ import mongoose from 'mongoose';
 const {Schema} = mongoose;
 
 const eventSchema = new Schema({
-    name: String,
-    attachments: [{base64: String, date: {type: Date, default: Date.now}}],
+    name: {
+        type: String,
+        required: true
+    },
+    description: String,
+    logo: String, //base64
+    attachments: [{
+        base64: {type: String, required: true},
+        date: {type: Date, default: Date.now}
+    }],
+    comments: [{
+        creator: {type: String, required: true},
+        body: {type: String, required: true},
+        date: {type: Date, default: Date.now}
+    }],
+    date: {type: Date, required: true},
     createTs: {type: Date, default: Date.now},
-    comments: [{creator: String, body: String, date: Date}],
-    createdBy: String,
-    rating: Number,
-    location:String
+    createdBy: {type: String, required: true},
+    priority: {type: Number, default: 0},
+    viewCount: {type: Number, default: 0},
+    location: String
 });
 
 const EventModel = mongoose.model('Events', eventSchema);
